38a8a4a0e94825fac1a57fa7d53d1e05c8500f7a
Alex Morgan <c3331544@uon.edu.au>
2019-04-04 05:20:15 +0000

    ...

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 
 int main () {
+    char input[] = "Encrypt this!";
+    int i=0;
+    int n = sizeof(input)/sizeof(char); // end point 
+    char output[100];
+    
+    
+    
+    for (input)
+    
+        if ()
+    
+    /* TO DO: Loop thorough all the elements */
+   /* Pass element to encryption function */
+  /* Check if LC */
+ /* If LC, make UC */
+/* If UC, encrypt */
+/* Else do nothing */
+/* pass encrypted back */
+
     char a = 'A';
     char a2;
     a2 = a + 7;
     printf("The letter %c has ASCII value %d", a2, a2);
     return 0;
 }
+
