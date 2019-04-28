0bc26569a1aa3ebe2175b3b873560dcce219de74
Alex Morgan <c3331544@uon.edu.au>
2019-04-28 14:28:27 +0000

    Comments

--- main.c
+++ main.c
@@ -5,9 +5,10 @@
 
 int main () {
    
+    //Lowercase and uppercase chars are assigned an array value of 26 and the alphabet is initialised for both uppercase and lowercase letters.
     char uppercase[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     char lowercase[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    int userin;
+    int userin; //Variable userin is declared as user input
     
     printf("\nWELCOME TO THE ENCRYPTION AND DECRYPTION MENU\n"); // User friendly menu distinguishing the different tasks
     printf("1. Rotation Cipher Encryption\n");
@@ -37,7 +38,7 @@ int main () {
                     ch=msg1[i]; //The character variable is stored as msg1 varable.
                     if(ch>=97 && ch<=122) { //If the charachter is between 97 and 122 ASCII values, add key/rotation amount to the character.
         
-                        ch=ch+keyrot; //Add key/rotation amount to the character as stated above.
+                        ch=ch+keyrot; //Key/rotation amount is added to the character as stated above.
                         if(ch<97) { //If the character variable is less than the ASCII value of 97, rotate back to z.
             
                             ch=ch-26; //value of 26 is subtracted to rotate back to z.
@@ -45,19 +46,19 @@ int main () {
                     }
                     if(ch>=65 && ch<=90) { //If the character is between 65 and 90 ASCII values, add key/rotation amount to the character.
 
-                        ch=ch+keyrot;
+                        ch=ch+keyrot; //Key/rotation amount is added to the character as stated above.
                         if(ch<65) { //If the character is less than the ASCII value of 65 subtract 26, rotating the characters back to Z.
 
                             ch=ch-26; //value of 26 is subtracted to rotate back to Z.
                         }
                         if(ch>=91) { //If the character is greater than or equal to the special char ([), rotate back to A.
             
-                            ch=ch-26; //value of 26 is subtracted to rotate back to A.
+                            ch=ch-26; //Value of 26 is subtracted to rotate back to A.
                         }
                     }
-                    if(ch>=0 && ch<=64) { //if the ccharacter is between 0 and 64 ASCII values, dont encrypt as these are special characters.
+                    if(ch>=0 && ch<=64) { //If the character is between 0 and 64 ASCII values, dont encrypt as these are special characters.
         
-                        ch=ch;
+                        ch=ch; //Character variable is stored as unchanged character variable.
                     }
 
                     msg1[i]=ch; //The new encrypted message of size i has new character shift values for each letter in the message.
@@ -81,30 +82,30 @@ int main () {
                     
                     ch=msg2[i]; //The character variable is stored as msg2 varable.
         
-                    if(ch>=97 && ch<=122) {
+                    if(ch>=97 && ch<=122) { //If the character variable is between ASCII values of 97 and 122, add the key/rotation amount to the character.
         
                         ch=ch-keyrot;
-                        if(ch<97) {
+                        if(ch<97) { //If the character variable is less than 97, rotate cipher back to "z".
                         
-                            ch=ch+26;
+                            ch=ch+26; //value of 26 is added to rotate back to z.
                         }
                     }
-                    if(ch>=65 && ch<=90) {
+                    if(ch>=65 && ch<=90) { //If the character is between 65 and 90 ASCII values, subtract key/rotation amount to the character.
         
-                        ch=ch-keyrot;
-                        if(ch<65) {
+                        ch=ch-keyrot; //Key/rotation amount is subtracted to the character as stated above.
+                        if(ch<65) { //If the character is less than the ASCII value of 65 add 26, rotating the characters back to Z.
                 
-                            ch=ch+26;
+                            ch=ch+26; //Value of 26 is added to rotate back to Z.
                         }
     
-                        if(ch>=91) {
+                        if(ch>=91) { //If the character is greater than or equal to the special char ([), rotate back to A.
             
-                            ch=ch+26;
+                            ch=ch+26; //Value of 26 is added to rotate back to A.
                         }                        
                     }
-                    if(ch>=0 && ch<=64) {
+                    if(ch>=0 && ch<=64) { //If the character is between 0 and 64 ASCII values, dont encrypt as these are special characters.
             
-                        ch=ch;
+                        ch=ch; //Character variable is stored as unchanged character variable.
                         }
     
                         msg2[i]=ch; //The new decrypted message of size i has new character shift values for each letter in the message.
@@ -115,29 +116,29 @@ int main () {
         
         case 3: //Substitution Cipher Encryption
             {
-                char keysub[27], msg3[1500];
+                char keysub[27], msg3[1500]; //Keysub value is assigned memory value of 27. Memory value of 1500 is given to the message input initialised as msg3. 
                           
                 printf("\nEnter message to encrypt: "); //User is prompted to enter a message to encrypt in UPPERCASE letters.
-                scanf(" %[^\n]",msg3);
+                scanf(" %[^\n]",msg3); //User input is scanned for string msg3.
 
                 int n,i; //Initialising counter variables n and i to use for array size.
 
                 printf("\nEnter key/26 characters: "); //26 characters are entered in uppercase when prompted. Each character can only be used once.
                 scanf("%s",keysub); //Stores key input in keysub.
 
-                for(n=0;n<strlen(msg3);n++) {
+                for(n=0;n<strlen(msg3);n++) { //n is initialised to 0, while n is less than string length in text in "msg3", n is incremented by 1 each time.
                            
-                    for(i=0;i<26;i++) {
+                    for(i=0;i<26;i++) { //i is initialised to 0, while i is less than 26, i is incremented by 1 each time.
                                 
-                        if(uppercase[i]==msg3[n]) { 
+                        if(uppercase[i]==msg3[n]) { //if the message is uppercase, it will equal the key value of that letter.
                                   
-                            msg3[n]=keysub[i];
+                            msg3[n]=keysub[i]; //Each letter of msg3 is substituted for the keysub letters.
                             break;
                         }
-                        if(lowercase[i]==msg3[n]) {
+                        if(lowercase[i]==msg3[n]) { //if the message lowercase, it will equal the key value of that letter.
                             
-                            msg3[n]=keysub[i];
-                            break;
+                            msg3[n]=keysub[i]; //Each letter of msg3 is substituted for the keysub letters.
+                            break; //Loop is exited
                         }
                     }
                 }
@@ -147,19 +148,19 @@ int main () {
         
         case 4: //Substitution Cipher Decryption
             {
-                char keysub[27], msg4[1500];
+                char keysub[27], msg4[1500]; //Keysub value is assigned memory value of 27. Memory value of 1500 is given to the message input initialised as msg4.
                           
                 printf("\nEnter message to decrypt: "); //User is prompted to enter a message to decrypt in UPPERCASE letters.
-                scanf(" %[^\n]",msg4); //Stores key input in keysub.
+                scanf(" %[^\n]",msg4); //User input is scanned for string msg3.
 
                 int n,i; //Initialising counter variables n and i for array size.
 
                 printf("\nEnter key/26 characters: "); //26 characters are entered in uppercase when prompted. Each character can only be used once.
                 scanf("%s",keysub); //Stores key input in keysub.
                           
-                for(n=0;n<strlen(msg4);n++) {
+                for(n=0;n<strlen(msg4);n++) { //n is initialised to 0, while n is less than string length in text in "msg4", n is incremented by 1 each time
                             
-                    for(i=0;i<26;i++) {
+                    for(i=0;i<26;i++) { //i is initialised to 0, while i is less than 26, i is incremented by 1 each time.
                                 
                         if(msg4[n]==keysub[i]) {
                                     
